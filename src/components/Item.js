95367ca7ff45af2686173f36951f3a33cf763b1c
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-01-15 12:55:40 +0300

    Completed Assingment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [click,change] = useState(false)
+  function useClick(){
+    change((click) => !click)
+  }
   return (
-    <li className="">
+    <li className={click ?"in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={useClick}>{click? "Remove from Cart":"Add to Cart"}</button>
     </li>
   );
 }
